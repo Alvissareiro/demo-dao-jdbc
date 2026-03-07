d338786e2b02fda66b5c62788a7376143c7cf6f8
Caio Café <caiocafe58@gmail.com>
2026-03-07 15:39:39 -0300

    Seller created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,9 +11,11 @@ public class Program {
 		// TODO Auto-generated method stub
 
 		Department obj = new Department(1,  "books");
-		
+		Seller seller = new Seller(21, "Bob",  "bob@gmail.com", new Date(), 3000.00, obj);
 		System.out.println(obj);
-	
+		System.out.println(seller);	
+		
+		
 		
 		
 		
